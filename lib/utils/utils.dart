5f25d686020869f5b70b44b3af62bcea0a9342a8
Usaid Asif <usaidasif12@gmail.com>
2023-11-22 19:03:51 +0500

    Customize Toast Message

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,7 +1,13 @@
+import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 
 class Utils {
   static toastmessage(String message) {
-    Fluttertoast.showToast(msg: message);
+    Fluttertoast.showToast(
+      msg: message,
+      backgroundColor: Colors.blueGrey,
+      textColor: Colors.black,
+      toastLength: Toast.LENGTH_LONG,
+    );
   }
 }
