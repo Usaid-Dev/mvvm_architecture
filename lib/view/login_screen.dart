e2884353ab3d7a12dc01c3800acb4080397545f0
Usaid Asif <usaidasif12@gmail.com>
2023-12-02 17:22:20 +0500

    Handle Obsecure Password

--- lib/view/login_screen.dart
+++ lib/view/login_screen.dart
@@ -9,6 +9,8 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  ValueNotifier<bool> _obsecurePassword = ValueNotifier<bool>(true);
+
   TextEditingController _emailcontroller = TextEditingController();
   TextEditingController _passwordcontroller = TextEditingController();
 
@@ -17,44 +19,73 @@ class _LoginScreenState extends State<LoginScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final height = MediaQuery.of(context).size.height * 1;
     return Scaffold(
-        body: SafeArea(
-      child: Column(
-        children: [
-          TextFormField(
-            controller: _emailcontroller,
-            keyboardType: TextInputType.emailAddress,
-            focusNode: emailFocusNode,
-            decoration: const InputDecoration(
-              hintText: 'Email',
-              labelText: 'Email',
-              prefixIcon: Icon(
-                Icons.alternate_email,
-              ),
-            ),
-            onFieldSubmitted: (value) {
-              Utils.fieldFocusChange(
-                  context, emailFocusNode, passwordFocusNode);
-            },
+      appBar: AppBar(
+        title: const Text(
+          'Login Screen',
+          style: TextStyle(
+            color: Colors.white,
           ),
-          TextFormField(
-            controller: _passwordcontroller,
-            focusNode: passwordFocusNode,
-            obscureText: true,
-            obscuringCharacter: '*',
-            decoration: const InputDecoration(
-              hintText: 'Password',
-              labelText: 'Password',
-              prefixIcon: Icon(
-                Icons.alternate_email,
-              ),
-              suffixIcon: Icon(
-                Icons.visibility_off_outlined,
-              ),
-            ),
-          ),
-        ],
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.blue,
       ),
-    ));
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            TextFormField(
+              controller: _emailcontroller,
+              keyboardType: TextInputType.emailAddress,
+              focusNode: emailFocusNode,
+              decoration: const InputDecoration(
+                hintText: 'Email',
+                labelText: 'Email',
+                prefixIcon: Icon(
+                  Icons.alternate_email,
+                ),
+              ),
+              onFieldSubmitted: (value) {
+                Utils.fieldFocusChange(
+                    context, emailFocusNode, passwordFocusNode);
+              },
+            ),
+            ValueListenableBuilder(
+              valueListenable: _obsecurePassword,
+              builder: (context, value, child) {
+                return TextFormField(
+                  controller: _passwordcontroller,
+                  obscureText: _obsecurePassword.value,
+                  focusNode: passwordFocusNode,
+                  obscuringCharacter: '*',
+                  decoration: InputDecoration(
+                    hintText: 'Password',
+                    labelText: 'Password',
+                    prefixIcon: const Icon(
+                      Icons.lock_open_rounded,
+                    ),
+                    suffixIcon: InkWell(
+                      onTap: () {
+                        _obsecurePassword.value = !_obsecurePassword.value;
+                      },
+                      child: Icon(
+                        _obsecurePassword.value
+                            ? Icons.visibility_off_outlined
+                            : Icons.visibility,
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+            SizedBox(
+              height: height * .1,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
