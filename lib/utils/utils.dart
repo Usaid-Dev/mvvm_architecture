a6f13b583e402e97c09937b1636bde22992f7ab4
Usaid Asif <usaidasif12@gmail.com>
2023-12-02 16:58:18 +0500

    Handle FocusNode

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -4,6 +4,15 @@ import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 
 class Utils {
+  static void fieldFocusChange(
+    BuildContext context,
+    FocusNode current,
+    FocusNode nextFocus,
+  ) {
+    current.unfocus();
+    FocusScope.of(context).requestFocus(nextFocus);
+  }
+
   static toastmessage(String message) {
     Fluttertoast.showToast(
       msg: message,
--- lib/view/login_screen.dart
+++ lib/view/login_screen.dart
@@ -9,16 +9,51 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  TextEditingController _emailcontroller = TextEditingController();
+  TextEditingController _passwordcontroller = TextEditingController();
+
+  FocusNode emailFocusNode = FocusNode();
+  FocusNode passwordFocusNode = FocusNode();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-      child: InkWell(
-        onTap: () {
-          Utils.snakBar('No internet connection', context);
-          // Utils.toastmessage('Hello World');
-        },
-        child: const Text('Click'),
+        body: SafeArea(
+      child: Column(
+        children: [
+          TextFormField(
+            controller: _emailcontroller,
+            keyboardType: TextInputType.emailAddress,
+            focusNode: emailFocusNode,
+            decoration: const InputDecoration(
+              hintText: 'Email',
+              labelText: 'Email',
+              prefixIcon: Icon(
+                Icons.alternate_email,
+              ),
+            ),
+            onFieldSubmitted: (value) {
+              Utils.fieldFocusChange(
+                  context, emailFocusNode, passwordFocusNode);
+            },
+          ),
+          TextFormField(
+            controller: _passwordcontroller,
+            focusNode: passwordFocusNode,
+            obscureText: true,
+            obscuringCharacter: '*',
+            decoration: const InputDecoration(
+              hintText: 'Password',
+              labelText: 'Password',
+              prefixIcon: Icon(
+                Icons.alternate_email,
+              ),
+              suffixIcon: Icon(
+                Icons.visibility_off_outlined,
+              ),
+            ),
+          ),
+        ],
       ),
     ));
   }
